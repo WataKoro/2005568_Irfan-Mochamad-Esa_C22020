f493a71fa47b40095060256aa08fbd87f7587a24
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-03-16 09:29:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "=================================". "<br>";
+echo "Nama : Irfan Mochamad Esa". "<br>";
+echo "NIM : 2005568". "<br>" ;
+echo "Ilmu Komputer C2 2020". "<br>";
+echo "=================================". "<br>";
+
+?>
